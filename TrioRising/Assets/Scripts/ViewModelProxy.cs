2987fd8ad554878bd2d44c24dcf5ffc087552adb
BDJMartinez <145031898+BDJMartinez@users.noreply.github.com>
2024-11-10 21:10:26 -0500

    Merged main into PlayerController.

--- TrioRising/Assets/Scripts/ViewModelProxy.cs
+++ TrioRising/Assets/Scripts/ViewModelProxy.cs
@@ -7,18 +7,5 @@ public class ViewModelProxy : MonoBehaviour
 {
     private PlayerController player; // Refrence the player holding the View Model
 
-    // Start is called before the first frame update
-    private void StartReload()
-    {
-        if (player.HeldWeapon)
-        {
-
-        }
-    }
-
-    // Update is called once per frame
-    private void EndReload()
-    {
-        
-    }
+ 
 }
